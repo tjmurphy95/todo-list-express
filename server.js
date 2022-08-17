9df7bc607136872941c25ef8f0c2a84ad6b466c9
snurffy <thomas.murph1995@gmail.com>
2022-08-17 15:02:34 +1000

    inital commit, basic express routes and styling

---
+++ server.js
@@ -0,0 +1,112 @@
+//import express
+const express = require("express");
+//initialize app by calling express
+const app = express();
+//connect
+//import mongoclient
+const MongoClient = require("mongodb").MongoClient;
+// set port to env value or defualt 2121
+const PORT = process.env.PORT || 2121;
+//import env
+const dotenv = require("dotenv");
+
+//Load config env - specify path
+dotenv.config({ path: "./config/config.env" });
+
+let db,
+  dbConnectionStr = process.env.MONGO_URI;
+dbName = "todo";
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }).then(
+  (client) => {
+    console.log(`Connected to ${dbName} Database`);
+    db = client.db(dbName);
+  }
+);
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", async (request, response) => {
+  const todoItems = await db.collection("todos").find().toArray();
+  const itemsLeft = await db
+    .collection("todos")
+    .countDocuments({ completed: false });
+  response.render("index.ejs", { items: todoItems, left: itemsLeft });
+  // db.collection('todos').find().toArray()
+  // .then(data => {
+  //     db.collection('todos').countDocuments({completed: false})
+  //     .then(itemsLeft => {
+  //         response.render('index.ejs', { items: data, left: itemsLeft })
+  //     })
+  // })
+  // .catch(error => console.error(error))
+});
+
+app.post("/addTodo", (request, response) => {
+  db.collection("todos")
+    .insertOne({ thing: request.body.todoItem, completed: false })
+    .then((result) => {
+      console.log("Todo Added");
+      response.redirect("/");
+    })
+    .catch((error) => console.error(error));
+});
+
+app.put("/markComplete", (request, response) => {
+  db.collection("todos")
+    .updateOne(
+      { thing: request.body.itemFromJS },
+      {
+        $set: {
+          completed: true,
+        },
+      },
+      {
+        sort: { _id: -1 },
+        upsert: false,
+      }
+    )
+    .then((result) => {
+      console.log("Marked Complete");
+      response.json("Marked Complete");
+    })
+    .catch((error) => console.error(error));
+});
+
+app.put("/markUnComplete", (request, response) => {
+  db.collection("todos")
+    .updateOne(
+      { thing: request.body.itemFromJS },
+      {
+        $set: {
+          completed: false,
+        },
+      },
+      {
+        sort: { _id: -1 },
+        upsert: false,
+      }
+    )
+    .then((result) => {
+      console.log("Marked Complete");
+      response.json("Marked Complete");
+    })
+    .catch((error) => console.error(error));
+});
+
+app.delete("/deleteItem", (request, response) => {
+  db.collection("todos")
+    .deleteOne({ thing: request.body.itemFromJS })
+    .then((result) => {
+      console.log("Todo Deleted");
+      response.json("Todo Deleted");
+    })
+    .catch((error) => console.error(error));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
